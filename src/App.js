c6a56e50a6ef78d52962447a4c88ca4d3d707934
laughingor2018 <2006cheng@163.com>
2019-03-30 21:37:18 +0800

    Signed-off-by: laughingor2018 <2006cheng@163.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header"
 
 class App extends Component {
   render() {
     return (
         <div className="container">
+          <div className="row">
+            <div className="col-xs-1">
+              <Header/>
+            </div>
+          </div>
           <div className="row">
             <div className="col-xs-1">
               <h1>Hello World</h1>
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-1">
+                        <h1>Header</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Header;
